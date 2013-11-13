70a38aa3a8f458fb508d6f0e5b7051629991856e
Sean O'Hara <sohara@sohara.com>
2013-11-13 16:35:26 -0500

    Don't alias_method_chain finalize! in extension

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -11,18 +11,18 @@ Spree::Order.class_eval do
 
   # Finalizes an in progress order after checkout is complete.
   # Called after transition to complete state when payments will have been processed.
-  def finalize_with_gift_card!
-    finalize_without_gift_card!
-    # Send out emails for any newly purchased gift cards.
-    self.line_items.each do |li|
-      Spree::OrderMailer.gift_card_email(li.gift_card.id, self).deliver if li.gift_card
-    end
-    # Record any gift card redemptions.
-    self.adjustments.where(originator_type: 'Spree::GiftCard').each do |adjustment|
-      adjustment.originator.debit(adjustment.amount, self)
-    end
-  end
-  alias_method_chain :finalize!, :gift_card
+  # def finalize_with_gift_card!
+  #   finalize_without_gift_card!
+  #   # Send out emails for any newly purchased gift cards.
+  #   self.line_items.each do |li|
+  #     Spree::OrderMailer.gift_card_email(li.gift_card.id, self).deliver if li.gift_card
+  #   end
+  #   # Record any gift card redemptions.
+  #   self.adjustments.where(originator_type: 'Spree::GiftCard').each do |adjustment|
+  #     adjustment.originator.debit(adjustment.amount, self)
+  #   end
+  # end
+  # alias_method_chain :finalize!, :gift_card
 
   # Tells us if there is the specified gift code already associated with the order
   # regardless of whether or not its currently eligible.
