98676873769c47a8e57e09eb90af5b24a0943c9a
Sean O'Hara <sohara@sohara.com>
2014-02-20 15:04:54 -0500

    Don't durably decorate after_update_atributes

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -2,9 +2,9 @@ Spree::OrdersController.class_eval do
 
   Spree::PermittedAttributes.checkout_attributes << :gift_code
 
-  durably_decorate :after_update_attributes, mode: 'soft', sha: 'bdc8fc02ee53912eda684bdd37a6266594665866' do
-    apply_gift_code
-    original_after_update_attributes
-  end
+  # durably_decorate :after_update_attributes, mode: 'soft', sha: 'bdc8fc02ee53912eda684bdd37a6266594665866' do
+  #   apply_gift_code
+  #   original_after_update_attributes
+  # end
 
 end
