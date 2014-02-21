674c3b704417f600336850ac9889d189e414e303
Sean O'Hara <sohara@sohara.com>
2014-02-21 11:34:40 -0500

    Replace other occurences of 'orginator' with source for adjustment

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -9,9 +9,9 @@ module Spree
       end
 
       def compute(order, gift_card)
-        # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and 
+        # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and
         # tax_total, minus other credits.
-        credits = order.adjustments.select{|a|a.amount < 0 && a.originator_type != 'Spree::GiftCard'}.map(&:amount).sum
+        credits = order.adjustments.select{|a|a.amount < 0 && a.source_type != 'Spree::GiftCard'}.map(&:amount).sum
         [(order.item_total + order.ship_total + order.tax_total + credits), gift_card.current_value].min * -1
       end
 
