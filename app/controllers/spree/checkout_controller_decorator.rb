2e72f02db663eb8b82af212af8c0ee6e21f61fea
Sean O'Hara <sohara@sohara.com>
2014-02-25 18:24:11 -0500

    Get some feature specs passing

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,12 +2,12 @@ Spree::CheckoutController.class_eval do
 
   Spree::PermittedAttributes.checkout_attributes << :gift_code
 
-  durably_decorate :update, mode: 'soft', sha: '131d36c23333d439e6dea57fb311d878dd3838f3' do
-    if @order.update_attributes(object_params)
+  durably_decorate :update, mode: 'soft', sha: '908d25b70eff597d32ddad7876cd89d3683d6734' do
+    if @order.update_from_params(params, permitted_checkout_attributes)
+      persist_user_address
       if @order.gift_code.present?
         render :edit and return unless apply_gift_code
       end
-
       unless @order.next
         flash[:error] = @order.errors.full_messages.join("\n")
         redirect_to checkout_state_path(@order.state) and return
@@ -25,5 +25,6 @@ Spree::CheckoutController.class_eval do
       render :edit
     end
   end
+
 
 end
