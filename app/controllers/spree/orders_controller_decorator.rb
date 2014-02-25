ef682cd18476f823b1fa0f9713c8779e66649c67
Sean O'Hara <sohara@sohara.com>
2014-02-25 18:24:11 -0500

    Get some feature specs passing

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -2,9 +2,22 @@ Spree::OrdersController.class_eval do
 
   Spree::PermittedAttributes.checkout_attributes << :gift_code
 
-  # durably_decorate :after_update_attributes, mode: 'soft', sha: 'bdc8fc02ee53912eda684bdd37a6266594665866' do
-  #   apply_gift_code
-  #   original_after_update_attributes
-  # end
+  def update
+    if @order.contents.update_cart(order_params)
+      apply_gift_code
+      respond_with(@order) do |format|
+        format.html do
+          if params.has_key?(:checkout)
+            @order.next if @order.cart?
+            redirect_to checkout_state_path(@order.checkout_steps.first)
+          else
+            redirect_to cart_path
+          end
+        end
+      end
+    else
+      respond_with(@order)
+    end
+  end
 
 end
