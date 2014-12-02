fe9ae740fda2c7a07676ec7bb598ba0b28777ab2
Hector Bustillos <hector.bustillos@crowdint.com>
2014-12-02 14:33:31 -0600

    update adjustment correctly once it's applied

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -11,11 +11,21 @@ module Spree
       def compute(order, gift_card)
         # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and
         # tax_total, minus other credits.
+
+        #Need to Double check looks like it works but its pretty ugly
         current = order.adjustments.select{|a| a.source.try(:code) == gift_card.code}.first
-        return current.try(:amount) unless current.nil?
-        credits = order.adjustments.select{|a|a.amount < 0 && a.source_type != 'Spree::GiftCard' && a.source_type != 'Spree::PromotionAction'}.map(&:amount).sum
+        return current.try(:amount) unless current.nil? || (order.adjustment_total.to_f.abs + order.total.to_f) < gift_card.current_value.to_f
+
+        credits = order.adjustments.select{|a|a.amount < 0 && (a.source_type != 'Spree::GiftCard' || a.source_type != 'Spree::PromotionAction')}.map(&:amount).sum
+        credits_promo = order.adjustments.select{|a|a.amount < 0 && a.source_type == 'Spree::PromotionAction'}.map(&:amount).sum.abs
         credits_no_tax = order.adjustments.select{|a|a.amount < 0 && a.source_type != 'Spree::Tax'}.map(&:amount).sum
-        [(order.item_total + order.ship_total + order.tax_total + credits + credits_no_tax), gift_card.current_value].min * -1
+
+        if current && (order.adjustment_total.to_f.abs + order.total.to_f) < gift_card.current_value.to_f
+          credits_promo = order.adjustments.select{|a|a.source_type == 'Spree::PromotionAction'}.map(&:amount).sum
+          credits_no_tax = order.adjustments.select{|a|a.amount < 0 && a.source_type == 'Spree::Tax'}.map(&:amount).sum
+          credits = 0
+        end
+        [(order.item_total + order.ship_total + order.tax_total + credits + credits_promo + credits_no_tax), gift_card.current_value].min * -1
       end
 
     end
